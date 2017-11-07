996d2ab1be1946931e09349eb1636f71f48191a8
hedong <1016367040@qq.com>
2017-11-07 17:28:30 +0800

    1、EmptyLayout 处理

--- app/src/main/java/com/boomhe/mymvp/module/base/BaseActivity.java
+++ app/src/main/java/com/boomhe/mymvp/module/base/BaseActivity.java
@@ -1,13 +1,135 @@
 package com.boomhe.mymvp.module.base;
 
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+
+import com.boomhe.mymvp.R;
+import com.boomhe.mymvp.widget.EmptyLayout;
+import com.trello.rxlifecycle2.LifecycleTransformer;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @author boomhe on 2017/11/6.
  * 基类 Activity
  */
 
-public abstract class BaseActivity< T extends IBasePresenter > extends RxAppCompatActivity implements IBaseView {
+public abstract class BaseActivity< T extends IBasePresenter > extends RxAppCompatActivity implements IBaseView,EmptyLayout.OnRetryListener {
 
 
+    /**
+     * 把空视图放在基类统一处理，@Nullable 表明 View 可以为 null
+     */
+    @Nullable
+    @BindView(R.id.empty_layout)
+    protected EmptyLayout mEmptyLayout;
+
+    /**
+     * 把 Presenter 提取到基类需要配合基类的 initInjector() 进行注入，如果继承这个基类则必定要提供一个 Presenter 注入方法，
+     * 该APP所有 Presenter 都是在 Module 提供注入实现，也可以选择提供另外不带 Presenter 的基类
+     */
+    @Inject
+    protected T mPresenter;
+
+    /**
+     * 刷新控件
+     */
+    @Nullable
+    @BindView(R.id.swipe_refresh)
+    SwipeRefreshLayout mSwipeRefresh;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(attachLayoutRes());
+        ButterKnife.bind(this);
+
+        initInjector();
+        initViews();
+        initSwipeRefresh();
+        updateViews(false);
+    }
+
+    /**
+     * 绑定布局文件
+     * @return
+     */
+    @LayoutRes
+    protected abstract int attachLayoutRes();
+
+    /**
+     * Dagger 注入
+     */
+    protected abstract void initInjector();
+
+    /**
+     * 初始化视图
+     */
+    protected abstract void initViews();
+
+    /**
+     * 初始化下拉刷新
+     */
+    private void initSwipeRefresh(){
+        if (mSwipeRefresh != null) {
+
+        }
+    }
+
+    /**
+     * 更新视图控件
+     * @param b
+     */
+    protected abstract void updateViews(boolean b);
+
+    @Override
+    public void showLoading() {
+        if (mEmptyLayout != null) {
+            mEmptyLayout.setEmptyStatus(EmptyLayout.STATUS_LOADING);
+        }
+    }
+
+    @Override
+    public void hideLoading() {
+        if (mEmptyLayout != null) {
+            mEmptyLayout.hideView();
+        }
+    }
+
+    @Override
+    public void showNetError() {
+        if (mEmptyLayout != null) {
+            mEmptyLayout.setEmptyStatus(EmptyLayout.STATUS_NO_NET);
+            mEmptyLayout.setRetryListener(this);
+        }
+    }
+
+    @Override
+    public void finishRefresh() {
+        if (mSwipeRefresh != null) {
+            mSwipeRefresh.setRefreshing(false);
+        }
+
+    }
+
+    @Override
+    public <T> LifecycleTransformer<T> bindToLife() {
+        return this.bindToLifecycle();
+    }
+
+    @Override
+    public void onRetry() {
+        updateViews(false);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
--- app/src/main/java/com/boomhe/mymvp/widget/EmptyLayout.java
+++ app/src/main/java/com/boomhe/mymvp/widget/EmptyLayout.java
@@ -1,13 +1,29 @@
 package com.boomhe.mymvp.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.boomhe.mymvp.R;
+import com.github.ybq.android.spinkit.SpinKitView;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author boomhe on 2017/11/6.
+ * 空视图
  */
 
 public class EmptyLayout extends FrameLayout {
@@ -18,22 +34,108 @@ public class EmptyLayout extends FrameLayout {
     public static final int STATUS_NO_DATA = 3;
 
     private Context mContext;
+    private int mBgColor;
+    private int mEmptyStatus = STATUS_LOADING;
     private OnRetryListener mOnRetryListener;
+
+    @BindView(R.id.tv_net_error)
+    TextView mTvEmptyMessage;
+    @BindView(R.id.rl_empty_container)
+    View mFLEmptyContainer;
+    @BindView(R.id.empty_loading)
+    SpinKitView mEmptyLoading;
+    @BindView(R.id.empty_layout)
+    FrameLayout mEmptyLayout;
 
 
     public EmptyLayout(@NonNull Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public EmptyLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.mContext = context;
+        TypedArray array = mContext.obtainStyledAttributes(attrs, R.styleable.EmptyLayout);
+        mBgColor = array.getColor(R.styleable.EmptyLayout_background_color, Color.WHITE);
+        View.inflate(mContext, R.layout.layout_empty_loading, this);
+        ButterKnife.bind(this);
+        mEmptyLayout.setBackgroundColor(mBgColor);
+        array.recycle();
+
+        switchView();
+    }
+
+    /**
+     * 切换视图
+     */
+    private void switchView() {
+        switch (mEmptyStatus) {
+            case STATUS_LOADING:
+                setVisibility(VISIBLE);
+                mFLEmptyContainer.setVisibility(GONE);
+                mEmptyLayout.setVisibility(VISIBLE);
+                break;
+            case STATUS_NO_NET:
+            case STATUS_NO_DATA:
+                setVisibility(VISIBLE);
+                mFLEmptyContainer.setVisibility(VISIBLE);
+                mEmptyLayout.setVisibility(GONE);
+                break;
+            case STATUS_HIDE:
+                setVisibility(GONE);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 隐藏视图
+     */
+    public void hideView(){
+        mEmptyStatus = STATUS_HIDE;
+        switchView();
+    }
+
+    /**
+     * 设置状态
+     * @param emptyStatus
+     */
+    public void setEmptyStatus(@EmptyStatus int emptyStatus){
+        mEmptyStatus = emptyStatus;
+        switchView();
+    }
+
+    /**
+     * 获取状态
+     * @return
+     */
+    public int getEmptyStatus(){
+        return mEmptyStatus;
     }
 
     /**
      * 点击重试
      */
-    public interface OnRetryListener{
+    public interface OnRetryListener {
+        /**
+         * 重试
+         */
         void onRetry();
     }
+
+    public void setRetryListener(OnRetryListener retryListener){
+        this.mOnRetryListener = retryListener;
+    }
+
+    @OnClick(R.id.tv_net_error)
+    public void onClick(){
+        if (mOnRetryListener != null) {
+            mOnRetryListener.onRetry();
+        }
+    }
+
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({STATUS_LOADING,STATUS_NO_NET,STATUS_NO_DATA})
+    public @interface EmptyStatus{}
 }
